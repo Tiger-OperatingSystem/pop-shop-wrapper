9608bdd2b904d327767b692a856f497e14f45492
Natanael <natanaelgplay@gmail.com>
2023-01-26 08:08:50 -0300

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export LD_PRELOAD="/usr/lib/pop-things/shop/libunion.so:/usr/lib/pop-things/shop/libexec.so"
-#export UNION_PRELOAD="/usr/lib/pop-things/shop/"
+export UNION_PRELOAD="/usr/lib/pop-things/shop/"
 
 # Force GTK THEME
 
