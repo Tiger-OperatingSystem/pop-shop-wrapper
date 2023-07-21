b360bd6dc8efa86863d5561b0e412e89895db1f7
Natanael <natanaelgplay@gmail.com>
2023-07-21 09:41:54 -0300

    Update launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -5,7 +5,8 @@ export UNION_PRELOAD="/usr/lib/pop-things/shop/"
 
 # Force GTK THEME
 
-export DBUS_SESSION_BUS_ADDRESS=$(echo ${DBUS_SESSION_BUS_ADDRESS}| sed 's|/bus|/pop_bus|g')
+export $(dbus-launch)
+/usr/libexec/gvfsd &
 
 export GTK_THEME=Pop-dark
 io.elementary.appcenter ${@}
